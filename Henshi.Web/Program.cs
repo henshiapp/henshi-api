2d96a002f77051566a5129df34dc155128320e7a
neumanf <fabricionewman@protonmail.com>
2025-08-26 13:08:27 -0300

    feat: apply migrations of API startup

--- Henshi.Web/Program.cs
+++ Henshi.Web/Program.cs
@@ -1,8 +1,10 @@
 using System.Text.Json.Serialization;
 using Auth0.AspNetCore.Authentication;
+using Henshi.Flashcards.Infraestructure.Database;
 using Henshi.Flashcards.Infraestructure.Extensions;
 using Henshi.Flashcards.Presentation.Controllers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -33,6 +35,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<FlashcardDbContext>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+        var pendingMigrations = dbContext.Database.GetPendingMigrations();
+        if (pendingMigrations.Any())
+        {
+            logger.LogInformation("Applying pending migrations...");
+            dbContext.Database.Migrate();
+            logger.LogInformation("Migrations applied successfully.");
+        }
+        else
+        {
+            logger.LogInformation("No pending migrations found.");
+        }
+    }
+
     if (app.Environment.IsDevelopment())
     {
         app.MapOpenApi();
