1b80e058f4c30c0cf4cd25f1dee0a470c6ca2ce5
neumanf <fabricionewman@protonmail.com>
2025-09-13 20:43:54 -0300

    chore: remove encryption from question and answer columns due to search purposes

---
+++ Henshi.Flashcards/Data/Migrations/20250913232914_AddCollectionIdFk.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Henshi.Flashcards.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollectionIdFk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_flashcards_collection_id",
+                schema: "flashcards",
+                table: "flashcards",
+                column: "collection_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_flashcards_flashcard_collections_collection_id",
+                schema: "flashcards",
+                table: "flashcards",
+                column: "collection_id",
+                principalSchema: "flashcards",
+                principalTable: "flashcard_collections",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_flashcards_flashcard_collections_collection_id",
+                schema: "flashcards",
+                table: "flashcards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_flashcards_collection_id",
+                schema: "flashcards",
+                table: "flashcards");
+        }
+    }
+}
